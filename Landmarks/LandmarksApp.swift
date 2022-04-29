2c32d0340723b84903892573fc1b77b66d47164d
mollycaffery10 <mollycaffery10@gmail.com>
2022-04-28 21:27:28 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Molly Caffery on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
