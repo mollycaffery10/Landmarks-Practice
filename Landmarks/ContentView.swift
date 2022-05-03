75d7354b20f85d1cdf75bf421fe608fa346a1f0b
mollycaffery10 <mollycaffery10@gmail.com>
2022-05-02 21:00:56 -0400

    Combined content views and added a map view component. Part 1 is complete.

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,18 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading){
-            Text("Landscapes")
-                .font(.title)
-            .foregroundColor(Color.blue)
-            HStack {
-                Text("Fiordland National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("South Island")
+        VStack{
+            
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -100)
+                .padding(.bottom, -100)
+            
+            VStack(alignment: .leading){
+                Text("Landscapes")
+                    .font(.title)
+                    .foregroundColor(Color.blue)
+                HStack {
+                    Text("Fiordland National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("South Island")
+                        .font(.subheadline)
+                        .font(.subheadline)
+                }
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About")
+                    .font(.title2)
+                
+                Text("The Fiordland national park is at the southern most tip of the south island. It is surrounded by mountains and a great place to camp and hike.")
+                    .allowsTightening(true)
             }
+            .padding()
+            Spacer()
         }
-        .padding()
     }
 }
 
