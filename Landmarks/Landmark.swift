3efe2607e535cdeac36eca98fcf9e541b6f806d7
mollycaffery10 <mollycaffery10@gmail.com>
2022-05-03 20:27:25 -0400

    Starting to make the model to incorporate	with.

---
+++ Landmarks/Landmark.swift
@@ -0,0 +1,19 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Molly Caffery on 5/3/22.
+//
+
+import SwiftUI
+import Foundation
+
+struct Landmark: Hashable, Codable {
+    
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+}
